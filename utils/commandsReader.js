62a62355caac6c61e41f8d3460e8c21da01189fe
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2023-02-18 21:42:03 +0000

    custom file extension

--- utils/commandsReader.js
+++ utils/commandsReader.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs')
 
-module.exports.loadCommands = (bot, category = false, commandFile = "./commands") => {
+module.exports.loadCommands = (bot, category = false, extension=".aoi", commandFile = "./commands") => {
   if (!bot) throw new Error("AOIFile Error: Bot not defined");
   if (!commandFile) throw new Error("AOIFile Error: Please put a commands folder");
   if (category) {
@@ -9,11 +9,11 @@ module.exports.loadCommands = (bot, category = false, commandFile = "./commands"
     for (let category of categories) {
       const commands = fs.readdirSync(commandFile + "/" + category);
       for (let cmds of commands) {
-        const check = cmds.endsWith(".aoi");
+        const check = cmds.endsWith(extension);
         // if (!check) throw new Error(`${cmds} não termina com .aoi`)
-        const commandName = cmds.replace(".aoi", "");
+        const commandName = cmds.replace(extension, "");
         fs.readFile("./commands/" + category + "/" + cmds, 'utf8', (err, code) => {
-          if (!check) return console.error("AOIFile Error: " + commandName + " não termina com .aoi");
+          if (!check) return console.error("AOIFile Error: " + commandName + " não termina com " + extension);
           if (err) return console.error(err);
           console.log(`AOIFile Log: Loading ${commandName}...`)
           if (!code) return console.error(`AOIFile Error: o código do comando ${commandName} é só um vazio... apenas`)
@@ -29,11 +29,11 @@ module.exports.loadCommands = (bot, category = false, commandFile = "./commands"
   } else {
     const commands = fs.readdirSync(commandFile);
     for (let cmds of commands) {
-      const check = cmds.endsWith(".aoi");
+      const check = cmds.endsWith(extension);
       // if (!check) throw new Error(`${cmds} não termina com .aoi`)
-      const commandName = cmds.replace(".aoi", "");
+      const commandName = cmds.replace(extension, "");
       fs.readFile("./commands/" + cmds, 'utf8', (err, code) => {
-        if (!check) return console.error("AOIFile Error: " + commandName + " não termina com .aoi");
+        if (!check) return console.error("AOIFile Error: " + commandName + " não termina com " + extension);
         if (err) return console.error(err);
         console.log(`AOIFile Log: Loading ${commandName}...`)
         if (!code) return console.error(`AOIFile Error: o código do comando ${commandName} é só um vazio... apenas`)
